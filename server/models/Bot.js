199736ccb18c28f57c3399b10ef9688ae0ff633c
einfachnuraaron <einfachnursooooo@gmail.com>
2020-03-22 21:20:41 +0100

    Update Bot.js

--- server/models/Bot.js
+++ server/models/Bot.js
@@ -30,11 +30,13 @@ class Bot {
     // var playlistID = state.playlistID;
     // console.log(playlistID+"Aaron")
     // if(playlistID == null){
+    //Aaron
     // var playlistID = '37i9dQZF1EthkNN8NKPlxz';
+    //Luca
     var playlistID = '75qiwicEyZAKQ7vVcd1Kgm';
     //open.spotify.com/playlist/37i9dQZF1DWVWiyE9VDkCO?si=OGPdWfV_S9uSldDUzhllcw
-    https: // }
-    await getToken();
+    // }
+    https: await getToken();
     const res = await spotifyApi.getPlaylistTracks(playlistID);
     var rand = Math.floor(Math.random() * Math.floor(res.body.items.length));
     var track = res.body.items[rand].track;
